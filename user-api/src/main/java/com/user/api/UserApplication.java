f6d2066c6b1372a6422b4c023bc412b4e6864a99
Remya Philipose <remyaphilipose@gmail.com>
2018-03-07 19:19:56 +1100

    first commit

---
+++ user-api/src/main/java/com/user/api/UserApplication.java
@@ -0,0 +1,32 @@
+package com.user.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@ComponentScan("com.user")
+@EntityScan(basePackages = "com.user.model")
+@EnableJpaRepositories("com.user.repository")
+@SpringBootApplication
+public class UserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+
+		};
+	}
+
+}
