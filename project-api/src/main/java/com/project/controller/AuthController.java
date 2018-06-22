a509c87227dac10eb117a0e430b69c9c87e6659c
Remya Philipose <remyaphilipose@gmail.com>
2018-06-22 21:38:36 +1000

    working basic auth

--- project-api/src/main/java/com/project/controller/AuthController.java
+++ project-api/src/main/java/com/project/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.project.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.repository.JdbcRepository;
-import com.user.model.User;
 
 @RestController
 @RequestMapping("/")
@@ -16,11 +17,25 @@ public class AuthController {
 	@Autowired
 	public JdbcRepository jdbcRepository;
 	
-	@RequestMapping(path = "user/{email}/{passwordHash}",method = RequestMethod.GET)
-	public User getAuth(@PathVariable("email") String email,@PathVariable("passwordHash") String passwordHash){
-		return jdbcRepository.getUser(email, passwordHash);
-		
+	@RequestMapping(path = "login",method = RequestMethod.GET)
+	public ResponseEntity getAuth(){
+		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(path = "notlogin",method = RequestMethod.GET)
+	public void getBlah(){
+		System.out.println("Hit the secure method!!");	
+	}
+	
+	@RequestMapping(path = "secured",method = RequestMethod.GET)
+	public void getBlah2(HttpSession session){
+		System.out.println(session.getId());
+		
+		System.out.println("Hit the secure method!!");	
+	}
+	
+	
+	
 	
 	
 
--- project-api/src/main/java/com/project/security/SecurityConfiguration.java
+++ project-api/src/main/java/com/project/security/SecurityConfiguration.java
@@ -1,38 +1,47 @@
 package com.project.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	
-	@Autowired
-	private PromanAuthenticationProvider authenticationProvider;
-	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		auth.authenticationProvider(authenticationProvider);
-	}
-
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//		auth.inMemoryAuthentication()
-//		.withUser("test2@email.com").password("test").roles("Admin");
-//	}
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		//httpSecurity.addFilterBefore(new CorsFilter(null),ChannelProcessingFilter.class);
-		httpSecurity.authorizeRequests().anyRequest()
-		//.permitAll()
-		.fullyAuthenticated()
-		.and().httpBasic();
-		httpSecurity.csrf().disable();
-	}
-
-	
+ 
+	 @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.inMemoryAuthentication()
+	          .withUser("user").password("pass")
+	          .authorities("ROLE_USER");
+	    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+          .antMatchers("/login").permitAll()
+          .anyRequest().authenticated()
+          .and()
+          .httpBasic();
+    }
+    
+//    @Bean
+//    public AuthenticationEntryPoint loginUrlauthenticationEntryPoint(){
+//        return new LoginUrlAuthenticationEntryPoint("/login");
+//    }
+    
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
+    
 }
