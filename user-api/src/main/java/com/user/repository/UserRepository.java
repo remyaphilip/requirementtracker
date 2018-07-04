e466210c213c36f65fe79f9283c66353f12e4a39
Remya Philipose <remyaphilipose@gmail.com>
2018-07-04 19:12:45 +1000

    user repo updated

--- user-api/src/main/java/com/user/repository/UserRepository.java
+++ user-api/src/main/java/com/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.user.model.User;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+	public User findByEmail(String email);
+	public List<User> findByOrganisation(String organisation);
 
 }
