1cc9f8985d6332893c504e678f5b776b00e3ac24
Remya Philipose <remyaphilipose@gmail.com>
2018-07-08 20:28:14 +1000

    security

--- issuetrack-api/src/main/java/com/issuetrack/repository/IssueRepository.java
+++ issuetrack-api/src/main/java/com/issuetrack/repository/IssueRepository.java
@@ -1,9 +1,12 @@
 package com.issuetrack.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.issuetrack.model.Issue;
 
 public interface IssueRepository extends JpaRepository<Issue, Integer> {
+	public List<Issue> findByprojectId(List<Integer> projectId);
 
 }
